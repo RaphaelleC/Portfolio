53cab44b313d84b4820dbfce5aaf60ea28e3f99e
RaphaelleC <raphaelle.charrier@gmail.com>
2021-07-24 15:33:44 +0100

    complete version

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -10,9 +10,16 @@ function Interests() {
           <hr className="border"></hr>
         </div>
       </div>
-      <img src="" alt=""/>
       <div className="m-5">
         <p className="fs-5">I&apos;ve had the chance to travel in a few countries, like Japan, Spain (Barcelona), Italy (Rimini) and in the French Alps (I thrive when snowboarding). I am looking forward to my next adventures ! Discovering new ways of living, thinking and approaching life is such an enriching experience.</p>
+      </div>
+      <div className="row">
+        <div className="col">
+          <img src="https://i.imgur.com/6NveE7t.jpg" alt="French Alps" className="picture"/>
+        </div>
+        <div className="col">
+          <img src="https://i.imgur.com/1a9wO2v.jpg" alt="Sensoji Temple" className="picture"/>
+        </div>
       </div>
     </main>
   )
